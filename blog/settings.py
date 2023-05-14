b3704efd7af3d6d504cfdc04268ebf466ec901f5
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2023-05-14 21:28:17 +0100

    redeployment

--- blog/settings.py
+++ blog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.vercel.app']
 AUTH_USER_MODEL = 'auths.User'
 
 
